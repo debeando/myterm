f6b9abff09a5561c464aa8d13aede544205db282
Nicola Strappazzon <nicola51980@nicola51980.local>
2013-05-18 19:09:53 +0200

    My initial commit.

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Remove old link´s.
+if [ -f ~/.bash_profile ]; then
+  unlink ~/.bash_profile
+fi
+
+if [ -f ~/.vimrc ]; then
+  unlink ~/.vimrc
+fi
+
+if [ -f ~/.gitignore_global ]; then
+  unlink ~/.gitignore_global
+fi
+
+# Create link´s.
+ln -s ~/.myterm/bash/dotfiles/bash_profile ~/.bash_profile
+ln -s ~/.myterm/bash/dotfiles/vimrc ~/.vimrc
+ln -s ~/.myterm/bash/dotfiles/gitignore_global ~/.gitignore_global
+
+# Reload profile.
+source ~/.bash_profile
