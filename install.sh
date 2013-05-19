ac70aa65e71d231ed194fe40b8187f7d8dfeb063
Nicola Strappazzon <nicola51980@nicola51980.local>
2013-05-19 23:34:04 +0200

    Remove alias.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Remove old link´s.
+# ===============
 if [ -f ~/.bash_profile ]; then
   unlink ~/.bash_profile
 fi
@@ -14,12 +15,20 @@ if [ -f ~/.gitignore_global ]; then
 fi
 
 # Create link´s.
+# ===============
 ln -s ~/.myterm/bash/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.myterm/bash/dotfiles/vimrc ~/.vimrc
 ln -s ~/.myterm/bash/dotfiles/gitignore_global ~/.gitignore_global
 
 # Run several config commands.
+# ============================
+
+# Configure git:
 git config --global core.excludesfile ~/.gitignore_global
 
+# Open Terminal.app to configure theme.
+open .myterm/bash/theme/Solarized\ Dark\ ansi.terminal
+
 # Reload profile.
+# ===============
 source ~/.bash_profile
