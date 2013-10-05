e24bed59a6a8a5e056dc7e17d93105465a900874
Nicola Strappazzon C <nicola51980@gmail.com>
2013-10-05 12:09:29 +0200

    Change version for SublimeText install script.

--- configs/st2.sh
+++ configs/st2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/bin/subl
-rm ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
-ln -s ~/.myterm/configs/st2/settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+ln -s "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl" /usr/bin/subl
+if [ -f ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ]; then
+rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+fi
+ln -s ~/.myterm/configs/st2/settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
