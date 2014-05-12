2b184f504e0c1b08c14b8205db55b75eac14fda0
Nicola Strappazzon <nicola@wuaki.tv>
2014-05-12 17:20:40 +0200

    Add iTerm2 functions.

---
+++ bash/scripts/bash_profile/iterm.sh
@@ -0,0 +1,15 @@
+# iTerm
+# -----
+name() {
+  export PROMPT_COMMAND="echo -ne '\033]0;$1\007'"
+}
+
+tab-color() {
+    echo -ne "\033]6;1;bg;red;brightness;$1\a"
+    echo -ne "\033]6;1;bg;green;brightness;$2\a"
+    echo -ne "\033]6;1;bg;blue;brightness;$3\a"
+}
+
+tab-reset() {
+    echo -ne "\033]6;1;bg;*;default\a"
+}
