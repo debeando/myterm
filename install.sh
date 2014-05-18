595ee2248e9aa452ae00254c742de13bbc74b117
Nicola Strappazzon C <nicola51980@gmail.com>
2014-05-18 22:46:10 +0200

    Remove tmux.

--- install.sh
+++ install.sh
@@ -30,10 +30,6 @@ if [ -f ~/.editrc ]; then
   unlink ~/.editrc
 fi
 
-if [ -f ~/.tmux.conf ]; then
-  unlink ~/.tmux.conf
-fi
-
 # Create link´s.
 # ===============
 ln -s ~/.myterm/bash/dotfiles/bash_profile ~/.bash_profile
@@ -43,7 +39,6 @@ ln -s ~/.myterm/bash/dotfiles/vim/ ~/.vim
 ln -s ~/.myterm/bash/dotfiles/vimrc ~/.vimrc
 ln -s ~/.myterm/bash/dotfiles/ssh_config ~/.ssh/config
 ln -s ~/.myterm/bash/dotfiles/gitignore_global ~/.gitignore_global
-ln -s ~/.myterm/bash/dotfiles/tmux ~/.tmux.conf
 ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /bin/subl
 
 # Run several config commands.
