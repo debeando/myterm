ad31d7859861a36b785c72bdb583a68446217b5f
Nicola Strappazzon C <nicola51980@gmail.com>
2014-07-05 01:51:33 +0200

    Set SublimeText3

--- configs/st.sh
+++ configs/st.sh
@@ -5,7 +5,7 @@ ln -s "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl" /usr/bin
 
 # Remove current settings and link my custom settings:
 if [ -f ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ]; then
-rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+  rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 fi
 ln -s ~/.myterm/configs/st2/settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
